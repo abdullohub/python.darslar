da5ed14f8eae1dd6b2bb6d8b304229a5dd0dd0fb
abdullohub <daler200310@gmail.com>
2025-06-24 15:31:26 +0500

    data type input
    Men bugun data type input urgandim

---
+++ data_type_input.py
@@ -0,0 +1,24 @@
+# a = 4
+# b = 6
+# print(a+b)
+# ism='Abdulloh'
+# familiya='Karimjonov'
+# print (f'mening ismim {ism } familiyam {familiya}  {ism + familiya}')
+# a = 3.2
+# b = 2
+# print (f'{a+b},{a-b},{a*b},{a/b}, {a//b}')
+# yolgon=False
+# tugri=True
+# print  (f'{yolgon} {tugri}')
+# sm=metrga utish
+# a= 1025
+# b= a//100
+# print(b)
+# g=kg almashinuv
+g = 5000
+kg = g//1000
+print(kg)
+a=input('son kiriting:')
+print(a)
+b=29
+print(b)
